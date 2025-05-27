685169105d66187e7364e48f1839731be7f44ff8
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-05-27 11:32:36 +0900

    config 추가

---
+++ src/main/java/org/example/bootpromcompose/MetricsConfig.java
@@ -0,0 +1,16 @@
+package org.example.bootpromcompose;
+
+
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry registry) {
+        return new CountedAspect(registry);
+    }
+}
