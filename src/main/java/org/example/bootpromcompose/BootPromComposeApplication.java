e4c9e0db22e454be1b0d8aa7e2c1923396707f21
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-05-27 10:33:49 +0900

    기초 세팅

---
+++ src/main/java/org/example/bootpromcompose/BootPromComposeApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootpromcompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPromComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootPromComposeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootpromcompose/BootPromComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootpromcompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPromComposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
